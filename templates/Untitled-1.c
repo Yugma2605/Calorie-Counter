f6d976eb7ea5ac4d17ed53550f6b094b73dde33c
Yugma42 <ypatel42@asu.edu>
2023-09-21 00:33:09 -0700

    Initial commit

---
+++ templates/Untitled-1.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+
+int main(){
+    int num[5], i, result, sum;
+
+    printf("Enter 5 numbers: ");
+    for(i=0; i<5; i++){
+
+        scanf("%d", &num[]);
+
+    }
+    for(i=0; i<5; i++){
+
+        sum = num[i] + num[i+1];
+
+    }
+    result = sum/5;
+    printf("%d", result);
+
+    return 0;
+}
